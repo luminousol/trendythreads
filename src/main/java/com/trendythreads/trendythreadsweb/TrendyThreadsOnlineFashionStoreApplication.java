73f1dd2042a3603509d77d916009ec56d14a6982
luminousol <luminousolding@gmail.com>
2024-01-10 13:18:54 +0900

    ✨feat: 초기 세팅

---
+++ src/main/java/com/trendythreads/trendythreadsweb/TrendyThreadsOnlineFashionStoreApplication.java
@@ -0,0 +1,13 @@
+package com.trendythreads.trendythreadsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendyThreadsOnlineFashionStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendyThreadsOnlineFashionStoreApplication.class, args);
+	}
+
+}
