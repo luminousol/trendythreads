ca8f8d3996540d76974deab59bc13df3369c0c83
luminousol <luminousolding@gmail.com>
2024-02-06 14:37:42 +0900

    refactor : 코드 리팩토링

--- src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
+++ src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String helloController() {
         return "hello";
     }
