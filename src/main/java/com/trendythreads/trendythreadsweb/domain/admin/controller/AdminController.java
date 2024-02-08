cf0449dc74f527cf8d7c57b53714a751a44ee3be
luminousol <luminousolding@gmail.com>
2024-02-08 10:23:32 +0900

    feat : AdminController 추가

--- src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
+++ src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
@@ -4,7 +4,7 @@ package com.trendythreads.trendythreadsweb.domain.admin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("admins")
 public class AdminController {
 
     @GetMapping("/")
