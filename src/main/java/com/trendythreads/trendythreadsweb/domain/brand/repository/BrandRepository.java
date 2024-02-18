51b0472309e7eaf8af24d111708e075387bdbc7e
luminousol <luminousolding@gmail.com>
2024-02-18 20:28:50 +0900

    feat: Brand repository 추가

--- src/main/java/com/trendythreads/trendythreadsweb/domain/brand/repository/BrandRepository.java
+++ src/main/java/com/trendythreads/trendythreadsweb/domain/brand/repository/BrandRepository.java
@@ -3,6 +3,10 @@ package com.trendythreads.trendythreadsweb.domain.brand.repository;
 import com.trendythreads.trendythreadsweb.domain.brand.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    List<Brand> findByBrandBusinessNumber(String businessNumber);
 
 }
