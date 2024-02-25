f057f84889b56a34dcfcf80814273475ad6cd401
luminousol <luminousolding@gmail.com>
2024-02-26 01:07:55 +0900

    comment : admincontroller 코멘트 추가

--- src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
+++ src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
@@ -4,6 +4,12 @@ package com.trendythreads.trendythreadsweb.domain.admin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+admin 페이지의 역할
+1. 모든 brand, member 의 정보를 조회하고 삭제, 초기화 할 수 있다.
+ */
+
+
 @RestController("admins")
 public class AdminController {
 
@@ -11,5 +17,7 @@ public class AdminController {
     public String helloController() {
         return "hello";
     }
+
+
 
 }
