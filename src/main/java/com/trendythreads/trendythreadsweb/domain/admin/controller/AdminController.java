30917145370f6fbb8a8aaf2ec6193f2412c50804
luminousol <luminousolding@gmail.com>
2024-03-25 21:23:48 +0900

    feat : admincontroller

--- src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
+++ src/main/java/com/trendythreads/trendythreadsweb/domain/admin/controller/AdminController.java
@@ -1,7 +1,12 @@
 package com.trendythreads.trendythreadsweb.domain.admin.controller;
 
 
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -18,6 +23,28 @@ public class AdminController {
         return "hello";
     }
 
+//    @GetMapping("/brands")
+//    public ResponseEntity getBrands(){
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
 
+//    @GetMapping("/members")
+//    public ResponseEntity getMembers(){
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
+
+//    @DeleteMapping("/{brand-id}")
+//    public ResponseEntity deleteMember(@PathVariable("brand-id") Long brandId) {
+//        // brand 자체를 지우는 것이 맞을지..? 정보만 지워주는 게 맞을지..?
+//        brandService.deleteBrand(brandId);
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//    }
+
+//    @DeleteMapping("/{member-id}")
+//    public ResponseEntity deleteBrand(@PathVariable("member-id") @Positive Long memberId) {
+//        // member 자체를 지우는 것이 맞을지..? 정보만 지워주는 게 맞을지..?
+//        memberService.deleteMember(memberId);
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//    }
 
 }
